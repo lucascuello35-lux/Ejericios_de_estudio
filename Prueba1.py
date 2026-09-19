2b450dfb840cc6e509e6eb77e531bf7ca764a5ac
lucascuello35-lux <lucascuello35@gmail.com>
2026-09-19 16:50:54 -0300

    prueba guaradar solo main

--- Prueba1.py
+++ Prueba1.py
@@ -1,4 +1,6 @@
 print("esta es una nueva actualizacion")
 print("esta es otra actualizacion")
 print("probamos nuevamente")
-print("prueba de guardar rama2")
+print("guardar solo main")
+print("anda")
+
